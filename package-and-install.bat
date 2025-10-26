abf36cae087aaed181b311b039000d34d493c82a
Tom Bartels <tbartels@flexport.com>
2025-10-26 20:41:09 +0100

    Enhance packaging and installation scripts to compile service to ES5, improve output messages for clarity, and ensure correct paths for packaging and installation.

--- package-and-install.bat
+++ package-and-install.bat
@@ -1,17 +1,18 @@
 @echo off
 
-echo "Compiling service"
+echo "Compiling service to ES5"
 cd tv-service
-CMD /C "%ProgramFiles%\nodejs\npm" run install
+CMD /C "%ProgramFiles%\nodejs\npm" install
 CMD /C "%ProgramFiles%\nodejs\npm" run build
-echo "Adding stuff"
+echo "Preparing dist folder for packaging"
 CMD /C "%ProgramFiles%\nodejs\npm" run postbuild-win
 cd ..
 
-echo "Packaging"
-CMD /C ares-package tv-app/ tv-service/
+echo "Packaging app and service (dist folder contains ES5-compiled service)"
+CMD /C ares-package tv-app/ tv-service/dist/
 
+echo "Installing to TV"
 CMD /C ares-install .\com.slg.lgtv2mqtt_0.0.1_all.ipk -d emulator
-echo "Installing"
 
+echo "Launching app"
 CMD /C ares-launch -d emulator com.slg.lgtv2mqtt
--- package-and-install.sh
+++ package-and-install.sh
@@ -1,13 +1,18 @@
-echo "Compiling service"
-cd tv-service || exit
+#!/bin/bash
+set -e
+
+echo "Compiling service to ES5"
+cd tv-service
 npm install
 npm run build
-echo "Adding stuff"
+echo "Preparing dist folder for packaging"
 npm run postbuild-linux
-cd .. || exit
+cd ..
 
-echo "Packaging"
-ares-package tv-app/ tv-service/
+echo "Packaging app and service (dist folder contains ES5-compiled service)"
+ares-package tv-app/ tv-service/dist/
 
-echo "Installing"
+echo "Installing to TV"
 ares-install com.slg.lgtv2mqtt_0.0.1_all.ipk -d tv
+
+echo "Done! Service compiled to ES5 and installed."
